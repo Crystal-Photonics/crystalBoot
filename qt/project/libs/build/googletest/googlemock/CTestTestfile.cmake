f52c63fccdbcf11d82a56d11acb1c6c4e40670d8
arne krüger <ak@crystal-photonics.com>
2016-10-05 18:54:44 +0200

    reparing merge

--- qt/project/libs/build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ark/entwicklung/qt/qProtocolAnalyzer/project/libs/googletest/googlemock
-# Build directory: C:/Users/ark/entwicklung/qt/qProtocolAnalyzer/project/libs/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
