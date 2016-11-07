0b26b7e4a3be09711309d4bb21d6c38e516c6a60
arne krüger <ak@crystal-photonics.com>
2016-11-07 17:59:00 +0100

    -image creation via commandline works

--- imageCreator/project/app/main.cpp
+++ imageCreator/project/app/main.cpp
@@ -1,11 +1,59 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+#include <QFileInfo>
+#include "imagecreatorsettings.h"
+#include "firmwareencoder.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    switch (argc)
+    {
+    case 1:
 
-    return a.exec();
+        w.show();
+        a.exec();
+        break;
+    case 2:
+    {
+        QString creationSettings(argv[1]);
+        if (QFileInfo(creationSettings).exists()){
+
+            ImageCreatorSettings imageCreatorSettings(NULL);
+
+
+            imageCreatorSettings.load(creationSettings);
+
+            FirmwareEncoder fwImageEncode(imageCreatorSettings);
+            bool result1,result2;
+            result1 = fwImageEncode.loadFirmwareData();
+            if (!result1){
+                qDebug() << "error loading firmware data";
+                exit(-1);
+            }
+            result2 = fwImageEncode.saveImage();
+            if (!result1){
+                qDebug() << "error saving firmware data";
+                exit(-1);
+            }
+            if (result1 && result2){
+                qDebug() << "created successfully";
+            }
+
+        }else{
+            qDebug() << "cant find file" << creationSettings;
+            exit(-1);
+        }
+
+
+    }
+        break;
+    default:
+        qDebug() << "Too many arguments";
+        exit(-1);
+    }
+
+    return 0;
 }
