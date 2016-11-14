648ae6364f531860ac13c7ce92dfd3803afacc9e
arne krüger <ak@crystal-photonics.com>
2016-11-14 15:45:56 +0100

    -moving example to other project

--- mcu_example/git.sh
+++
@@ -1,5 +0,0 @@
-"C:\Program Files (x86)\Git\bin\git" log -1 --format="format:#define GITHASH 0x%h"  > include/vc.h
-ECHO " " >> include/vc.h
-"C:\Program Files (x86)\Git\bin\git" log -1 --date=short --format="format:#define GITDATE \"%ad\"" >> include/vc.h
-ECHO " " >> include/vc.h
-"C:\Program Files (x86)\Git\bin\git" log -1 --date=short --format="format:#define GITUNIX %ct" >> include/vc.h
--- mcu_example/include/version_info.h
+++
@@ -1,8 +0,0 @@
-#ifndef VERSION_INFO
-#define VERSION_INFO
-
-
-#define VERSION_INFO_VERSION "1.0"
-#define VERSION_INFO_NAME	"crystal boot example"
-
-#endif
